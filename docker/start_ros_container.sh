8b1d34c4ab1d671d3dca371a3dbe800359aa591c
Mojiu <timeworld168@163.com>
2026-07-23 22:53:26 +0800

    feat(docker): add container start

---
+++ docker/start_ros_container.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+CONTAINER="${DOCKER_CONTAINER_NAME:-ros2_jazzy}"
+[[ "${1:-}" == --container ]] && { CONTAINER="${2:?}"; shift 2; }
+exec docker start "$@" "$CONTAINER"
